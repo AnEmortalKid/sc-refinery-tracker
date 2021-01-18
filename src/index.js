fab077ec6dda3bd5efcdb45fdb814a007c33afb1
AnEmortalKid <janmonterrubio@gmail.com>
2021-01-18 17:49:52 -0600

    pretty

--- src/index.js
+++ src/index.js
@@ -96,7 +96,7 @@ export function addUser() {
     // immediately switch
     userController.setUser(userName);
     userView.layout();
-    
+
     runController.loadRuns();
     runView.layout();
 
