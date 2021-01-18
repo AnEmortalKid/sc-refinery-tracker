498c72265014d306f2fdc6913bee0c77927b38b8
AnEmortalKid <janmonterrubio@gmail.com>
2021-01-18 12:56:33 -0600

    preserve work

--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,18 @@ entry: './src/index.js',
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'fonts/'
+            }
+          }
+        ]
+      }
     ],
   },
 };
