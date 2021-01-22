1a5aa65ab24ea64a8b18e37eacc9980f3171b85d
AnEmortalKid <janmonterrubio@gmail.com>
2021-01-21 21:44:23 -0600

    key controls

--- src/controls/controls.js
+++ src/controls/controls.js
@@ -6,4 +6,18 @@ export default class Controls {
   closeModal(modalId) {
     document.getElementById(modalId).style.display = "none";
   }
+
+  setEscapeClosesModals() {
+    document.addEventListener("keydown", (event) => {
+      if (event.key === "Escape") {
+        var modals = document.getElementsByClassName("w3-modal");
+        for (var i = 0; i < modals.length; i++) {
+          var modal = modals[i];
+          if (modal.style.display === "block") {
+            this.closeModal(modal.id);
+          }
+        }
+      }
+    });
+  }
 }
