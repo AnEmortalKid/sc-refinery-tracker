f80880eeca79305fa976def258bafeb72f6df9fb
AnEmortalKid <janmonterrubio@gmail.com>
2021-01-21 19:17:47 -0600

    rename

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
-  entry: "./src/index.js",
+  entry: "./src/app.js",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
