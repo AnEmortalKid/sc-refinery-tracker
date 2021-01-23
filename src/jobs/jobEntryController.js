61f7550f41eb86efe4ac3c8f03f81c4a639f053c
AnEmortalKid <janmonterrubio@gmail.com>
2021-01-23 11:11:51 -0600

    wip refactor entry

--- src/jobs/jobEntryController.js
+++ src/jobs/jobEntryController.js
@@ -1,11 +1,11 @@
-import { getMaterialsList } from "../model/materials";
-
 /**
  * Component that manages the entry and update of a Refinery Job
  */
 export default class JobEntryController {
   constructor(jobEntryView) {
     this.jobEntryView = jobEntryView;
+
+    this.jobEntryView.bindSubmitJob(this.handleJobEntry);
   }
 
   /**
@@ -15,7 +15,17 @@ export default class JobEntryController {
     this.jobEntryView.toggleMaterialEntryMode(event.target.checked);
   }
 
-  addMaterialEntry() {
+  addMaterialEntryOption() {
     this.jobEntryView.addMaterialOption();
+  }
+
+  /**
+   * Handles the submission of a job entry
+   * @param {object} jobEntry 
+   */
+  handleJobEntry(jobEntry) {
+    // convert entry data to Run
+    // this.model.addRun();
+    console.log('handleJobEntry');
   }
 }
--- src/jobs/jobEntryView.js
+++ src/jobs/jobEntryView.js
@@ -21,7 +21,24 @@ function createMaterialSelect(id) {
   return selectDiv;
 }
 
-function createMaterialInputValue(id, selectId) {
+function createRemoveMaterialInput(id) {
+  var removeMaterialDiv = document.createElement("div");
+  removeMaterialDiv.classList.add("w3-half");
+
+  var removeButton = document.createElement("button");
+  removeButton.classList.add("w3-btn", "w3-blue-grey", "w3-round-large");
+  removeButton.type = "button";
+  removeButton.onclick = () => document.getElementById(id).remove();
+
+  var icon = document.createElement("i");
+  icon.classList.add("fa", "fa-minus");
+  removeButton.appendChild(icon);
+  removeMaterialDiv.appendChild(removeButton);
+
+  return removeMaterialDiv;
+}
+
+function createMaterialInputValue(id, selectId, removable = true) {
   var materialEntryDiv = document.createElement("div");
   materialEntryDiv.classList.add("w3-third", "w3-row-padding");
 
@@ -36,21 +53,12 @@ function createMaterialInputValue(id, selectId) {
   materialInput.dataset.selectId = selectId;
   materialInputDiv.appendChild(materialInput);
 
-  var removeMaterialDiv = document.createElement("div");
-  removeMaterialDiv.classList.add("w3-half");
-  removeMaterialDiv.id = "remove.material.parent." + id;
-
-  var removeButton = document.createElement("button");
-  removeButton.classList.add("w3-btn", "w3-blue-grey", "w3-round-large");
-  removeButton.onclick = () => document.getElementById(id).remove();
-
-  var icon = document.createElement("i");
-  icon.classList.add("fa", "fa-minus");
-  removeButton.appendChild(icon);
-  removeMaterialDiv.appendChild(removeButton);
-
   materialEntryDiv.appendChild(materialInputDiv);
-  materialEntryDiv.appendChild(removeMaterialDiv);
+
+  if (removable) {
+    var removeMaterialDiv = createRemoveMaterialInput(id);
+    materialEntryDiv.appendChild(removeMaterialDiv);
+  }
 
   return materialEntryDiv;
 }
@@ -58,6 +66,10 @@ function createMaterialInputValue(id, selectId) {
  * Component responsible for managing the view portion of adding or editing refinery jobs
  */
 export default class JobEntryView {
+  constructor() {
+    this.form = document.getElementById("add-job-form");
+  }
+
   toggleMaterialEntryMode(toggleState) {
     var yieldRow = document.getElementById("yield-units");
     var materialRow = document.getElementById("material-units");
@@ -67,18 +79,16 @@ export default class JobEntryView {
 
     if (toggleState) {
       var container = document.getElementById("materials-container");
-      // if there's no material entries, create the first one and clean it up
+      
+      // if there's no material entries, create the first one
       if (container.children.length < 1) {
-        var firstEntryId = this.addMaterialOption();
-        // remove the delete button
-        document
-          .getElementById("remove.material.parent." + firstEntryId)
-          .remove();
+          // create option without ability to remove
+        this.addMaterialOption(false);
       }
     }
   }
 
-  addMaterialOption() {
+  addMaterialOption(removable = true) {
     var container = document.getElementById("materials-container");
 
     var rowId = new Date().getTime();
@@ -89,10 +99,54 @@ export default class JobEntryView {
     var selectDiv = createMaterialSelect(rowId);
     row.appendChild(selectDiv);
     row.appendChild(
-      createMaterialInputValue(rowId, selectDiv.dataset.selectId)
+      createMaterialInputValue(rowId, selectDiv.dataset.selectId, removable)
     );
     container.appendChild(row);
+  }
 
-    return rowId;
+  _getFormData() {
+
+    var form = document.getElementById("add-job-form");
+  var inputs = form.querySelectorAll("input");
+
+  var obj = {};
+  for (var i = 0; i < inputs.length; i++) {
+    var item = inputs.item(i);
+    if (item.name) {
+      obj[item.name] = item.value;
+    }
+  }
+
+  var selects = form.querySelectorAll("select");
+  for (var i = 0; i < selects.length; i++) {
+    var item = selects.item(i);
+    if (item.name) {
+      obj[item.name] = item.value;
+    }
+  }
+
+  }
+
+  /**
+   * Binds the submission of the Job to the given handler
+   * @param {function} handler a callback to call with the data for the form
+   */
+  bindSubmitJob(handler) {
+    this.form.addEventListener('submit', event => {
+      console.log('submit');
+      event.preventDefault();
+
+      // todo get form data
+      handler({});
+    });
+
+    // var btn = document.getElementById('add-job-form-confirm-btn');
+    // btn.addEventListener('click', event => {
+    //   console.log('OUCH');
+    // });
+  }
+
+  closeForm() {
+    // todo app.controls.close
   }
 }
